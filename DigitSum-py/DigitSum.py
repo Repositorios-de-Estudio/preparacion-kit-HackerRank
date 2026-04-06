4fbeffa0eb7629b6a2aa0aa8e32460340eca10a2
sergioPerez-e <dark07perez@gmail.com>
2026-04-06 21:59:31 +1000

    passed a half of the cases

--- DigitSum-py/DigitSum.py
+++ DigitSum-py/DigitSum.py
@@ -1,39 +1,80 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
 
 
 
-def convertToList(y):
-    ss = str(y)
+def convertToList(yy):
     nums = []
 
-    for e in ss:
+    for e in yy:
         nums.append(int(e))
     return nums
 
 
 def sumedigitos(mm):
+    print(">>> sumedigitos", mm)
+
     sumatoria = 0
 
     for e in mm:
-        sumatoria += e
+        sumatoria += int(e)
+    print(">>> sumedigitos, sumatoria: ", sumatoria)
 
     return sumatoria
 
-def digisum(x):
-    digitos = convertToList(x)
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+# Input x*k
+#  1. STRING x
+#  2. INTEGER k
+#
+def superDigit(x):
+    digitos = convertToList(str(x))
+    print(">>> digitos", digitos)
     result = 0
 
     if len(digitos) < 2:
+        print(">>> if", digitos)
+
         result = digitos[0]
     else:
+        print(">>> else", digitos)
         result = sumedigitos(digitos)
 
         if result > 9:
-            result = digisum(result)
+            result = superDigit(result)
 
     return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n*k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
 
 
 
 
 #print(digisum(9875))
-print(digisum(9875987598759875))
+#print(digisum(9875987598759875))
+#print(superDigit(str(9875)*4))
+#print(superDigit("9875987598759875"*16))
